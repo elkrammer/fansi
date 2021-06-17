f79078ff9d45db0596b87bb52638e0996f04fd8b
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2021-06-17 19:33:18 -0400

    map file into memory

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 void print_cp437();
+void load_ansi_file(const char *filename);
 
-int main() {
-    print_cp437();
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // print code page 437 table
+        print_cp437();
+    } else if (argc == 2) {
+        // load ansi file
+        load_ansi_file(argv[1]);
+    } else {
+        printf("Command error. Usage: ./fansi FILE");
+    }
     return 0;
 }
---
+++ src/parser.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+void load_ansi_file(const char *filename) {
+    char *buffer = NULL;
+    unsigned int buffer_size = 0;
+    int fd = open(filename, O_RDONLY); // file descriptor
+
+    if (fd < 0) {
+        fprintf(stderr,"Error: Unable to read ansi file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: validate this is actually an .ans file
+
+    buffer_size = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+    buffer = mmap(NULL, buffer_size, PROT_READ, MAP_SHARED, fd, 0);
+
+    // error reading file
+    if (buffer == (void*)(-1)) {
+        close(fd);
+        fprintf(stderr, "Error mapping file `%s' into memory.\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s", buffer);
+
+    // clean up
+    munmap(buffer, buffer_size);
+    close(fd);
+}
